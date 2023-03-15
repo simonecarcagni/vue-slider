f4a28fdf1c3f386dffcdd203198e77fe32af9133
Simone Carcagnì <simonecarcagni@libero.it>
2023-03-15 17:16:22 +0100

    Esercizio Completato

--- js/script.js
+++ js/script.js
@@ -1,25 +1,47 @@
-const slides = [
-    {
-        image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
-        image: 'img/02.webp',
-        title: 'Ratchet & Clank: Rift Apart',
-        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, {
-        image: 'img/03.webp',
-        title: 'Fortnite',
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
-        image: 'img/04.webp',
-        title: 'Stray',
-        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
-        image: 'img/05.webp',
-        title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-    }
-];
+const { createApp } = Vue
 
-console.log(slides);
+createApp({
+    data() {
+        return {
+            activeImage: 0,
+            images: ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp'],
+            titles: ['SpiderMan Miles Morales', 'Ratchet & Clank: Rift Apart', 'Fortnite', 'Stray', "Marvel's Avengers"],
+            texts: [
+                    'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.', 
+                    'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                    "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                    'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                    'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.'
+                    ],
+        }
+    },
+    methods: {
+        backButton() {
+            this.activeImage--;
+            if (this.activeImage < 0) {
+                this.activeImage = this.images.length - 1;
+            }
+        },
+        nextButton() {
+            this.activeImage++;
+            if (this.activeImage >= this.images.length) {
+                this.activeImage = 0;
+            }
+        },
+        showCurrentImage(currentActiveImage){
+            this.activeImage = currentActiveImage;
+        },
+        showActiveImage(index){
+            if(index == this.activeImage){
+                return 'active';
+            }
+            return '';
+        },
+        autoPlay(){
+            setInterval(this.nextButton, 3000);
+        }
+    },
+    mounted() {
+        this.autoPlay();
+    }
+}).mount('#app')
